e9b3d58726910a8bec9f4aab85d7a7d6f2b24f46
Guanghui He <hitpoint6@gmail.com>
2023-09-28 10:41:44 -0700

    first commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+    const router = useRouter();
+
+    const addMeetupHandler = async (enteredMeetupData) => {
+        console.log(enteredMeetupData);
+        const response = await fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+
+        router.push("/");
+    };
+
+    return (
+        <Fragment>
+            <Head>
+                <title>Add a New Meetup</title>
+                <meta name="description" content="Add your own meetups and create amazing networking opportunities." />
+            </Head>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </Fragment>
+    );
+}
+
+export default NewMeetupPage;
